ae79705f7aa86f2a03546d30e997286fe204bcd1
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-12-13 22:02:30 -0600

    trying to plot the axes

--- src/StdPlot2D.java
+++ src/StdPlot2D.java
@@ -8,8 +8,8 @@
  * 
  * @author <i> Tushar </i> 
  ******************************************************************************/
-
 /**
+ * The class containing the plot.
  * @author <i> Tushar </i>
  */
 public class StdPlot2D {
@@ -18,14 +18,22 @@ public class StdPlot2D {
      */
     private int canvasHeight;           
     private int canvasWidth;
-    private int xLower;
-    private int yLower;
-    private int xUpper;
-    private int yUpper;
-    private int xScale;
-    private int yScale;
-    private int xOffset;
-    private int yOffset;
+    private double xLower;
+    private double yLower;
+    private double xUpper;
+    private double yUpper;
+    private double xOffset;
+    private double yOffset;
+    private double yAxisOffset;
+    private double xAxisOffset;
+    private double trueXLower;
+    private double trueXUpper;
+    private double trueYUpper;
+    private double trueYLower;
+    private double majorXDiv;
+    private double minorXDiv;
+    private double majorYDiv;
+    private double minorYDiv;
     /**
      * Sets up a canvas with the given values.
      * @param values Order of values: 
@@ -39,26 +47,90 @@ public class StdPlot2D {
      * </ol>
      * @param provided provided[i] <=> The i<sup>th</sup> parameter has been provided.            
      */
-    public StdPlot2D(boolean[] provided, int[] values) {
+    public StdPlot2D(boolean[] provided, double[] values) {
         if (provided.length != 6) throw new IllegalArgumentException("You need 6 boolean values.");
         int current = 0;
-        canvasWidth = (provided[0]) ? values[current++] : 680;
-        canvasHeight = (provided[1]) ? values[current++] : 680;
+        canvasWidth = (provided[0]) ? (int) values[current++] : 680;
+        canvasHeight = (provided[1]) ? (int) values[current++] : 680;
         xLower = (provided[2]) ? values[current++] : 0;
         xUpper = (provided[3]) ? values[current++] : 1000;
         yLower = (provided[4]) ? values[current++] : 0;
         yUpper = (provided[5]) ? values[current++] : 1000;
-        StdDraw.setCanvasSize(canvasWidth, canvasHeight);
-        StdDraw.setXscale(xLower, xUpper);
-        StdDraw.setYscale(yLower, yUpper);
-        StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.line((xLower + xUpper)/2, yLower, (xLower + xUpper)/2, yUpper);
-        StdDraw.line(xLower, (yLower + yUpper)/2, xUpper, (yLower + yUpper)/2);
+        majorXDiv = majorYDiv = 100;
+        minorXDiv = minorYDiv = 10;
+        setScale();
     }
-        
+    /**
+     * Sets the scale for the X and Y axes.
+     */
+    public void setScale() {
+        boolean xRev = false;
+        StdDraw.setCanvasSize(canvasWidth, canvasHeight);
+        if (xLower > 0 && xUpper > 0) {                                          // Kink needed.
+            trueXLower = (xLower*canvasWidth - 25*xUpper) / (canvasWidth - 25);
+            trueXUpper = xUpper;
+            StdDraw.setXscale(trueXLower, trueXUpper);
+        }
+        else if (xLower == 0 && xUpper > 0) {                                    // No kink needed.
+            trueXLower = (xLower*canvasWidth - 15*xUpper) / (canvasWidth - 15);
+            trueXUpper = xUpper;
+            StdDraw.setXscale(trueXLower, trueXUpper);
+        }
+        else if (xLower < 0 && xUpper == 0) {                                    // No kink needed.
+            trueXLower = xLower;
+            trueXUpper = (xUpper*canvasWidth - 15*xLower) / (canvasWidth - 15);
+            StdDraw.setXscale(trueXLower, trueXUpper);
+            xRev = true;
+        }
+        else if (xLower < 0 && xUpper < 0) {                                     // Kink needed.
+            trueXLower = xLower;
+            trueXUpper = (xUpper*canvasWidth - 25*xLower) / (canvasWidth - 25);
+            StdDraw.setXscale(trueXLower, trueXUpper);
+            xRev = true;
+        }
+        if (yLower > 0 && yUpper > 0) {
+            trueYLower = (yLower*canvasHeight - 25*yUpper) / (canvasHeight - 25);
+            trueYUpper = yUpper;
+            StdDraw.setYscale(trueYLower, trueYUpper);
+            StdDraw.line(trueXLower, yLower, trueXUpper, yLower);
+        }
+        else if (yLower == 0 && yUpper > 0) {
+            trueYLower = (yLower*canvasHeight - 15*yUpper) / (canvasHeight - 15);
+            trueYUpper = yUpper;            
+            StdDraw.setYscale(trueYLower, trueYUpper);
+            StdDraw.line(trueXLower, yLower, trueXUpper, yLower);
+        }
+        else if (yLower < 0 && yUpper == 0) {
+            trueYLower = yLower;
+            trueYUpper = (yUpper*canvasHeight - 15*yLower) / (canvasHeight - 15);        
+            StdDraw.setYscale(trueYLower, trueYUpper);   
+            StdDraw.line(trueXLower, yUpper, trueXUpper, yUpper);
+        }
+        else if (yLower < 0 && yUpper < 0) {
+            trueYLower = yLower;
+            trueYUpper = (yUpper*canvasWidth - 25*xLower) / (canvasWidth - 25);
+            StdDraw.setYscale(trueYLower, trueYUpper);
+            StdDraw.line(trueXLower, yUpper, trueXUpper, yUpper);
+        }
+        if (xRev) 
+            StdDraw.line(xUpper, trueYLower, xUpper, trueYUpper);
+        else
+            StdDraw.line(xLower, trueYLower, xLower, trueYUpper);
+    }
+    /**
+     * Set the width represented by 1 major division along the x axis.
+     * @param majorDiv 
+     */
+    private void majorDiv(double majorDiv) {
+        this.majorXDiv = majorDiv;
+    }
+    /**
+     * For testing.
+     * @param args Conventional.
+     */
     public static void main(String[] args) {
-        boolean[] providing = {true, true, true, true, true, true};
-        int[] values = {600, 600, 0, 1000, 0, 1000};
+        boolean[] providing = {false, false, true, true, true, true};
+        double[] values = {-100, 1000, 20, 400};
         StdPlot2D plot = new StdPlot2D(providing, values);
     }
 }
