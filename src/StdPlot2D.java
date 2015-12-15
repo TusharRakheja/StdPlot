96215d8e945e5436acf6c4199fe8872f8a4136c8
tusharrakheja@gmail.com <tusharrakheja@gmail.com>
2015-12-14 20:53:28 -0600

    Changing the self-arranging BS.

--- src/StdPlot2D.java
+++ src/StdPlot2D.java
@@ -1,3 +1,6 @@
+
+import java.awt.Font;
+
 /*******************************************************************************
  * A basic 2D plotting library with an intuitive API. 
  * Uses StdDraw.java from <a href = http://introcs.cs.princeton.edu/java/stdlib/StdDraw.java.html>here</a>.
@@ -22,18 +25,12 @@ public class StdPlot2D {
     private double yLower;
     private double xUpper;
     private double yUpper;
-    private double xOffset;
-    private double yOffset;
-    private double yAxisOffset;
-    private double xAxisOffset;
-    private double trueXLower;
-    private double trueXUpper;
-    private double trueYUpper;
-    private double trueYLower;
     private double majorXDiv;
     private double minorXDiv;
     private double majorYDiv;
     private double minorYDiv;
+    private double xScale;
+    private double yScale;
     /**
      * Sets up a canvas with the given values.
      * @param values Order of values: 
@@ -50,72 +47,41 @@ public class StdPlot2D {
     public StdPlot2D(boolean[] provided, double[] values) {
         if (provided.length != 6) throw new IllegalArgumentException("You need 6 boolean values.");
         int current = 0;
-        canvasWidth = (provided[0]) ? (int) values[current++] : 680;
-        canvasHeight = (provided[1]) ? (int) values[current++] : 680;
+        canvasWidth = (provided[0]) ? (int) values[current++] : 800;
+        canvasHeight = (provided[1]) ? (int) values[current++] : 600;
         xLower = (provided[2]) ? values[current++] : 0;
         xUpper = (provided[3]) ? values[current++] : 1000;
         yLower = (provided[4]) ? values[current++] : 0;
         yUpper = (provided[5]) ? values[current++] : 1000;
         majorXDiv = majorYDiv = 100;
         minorXDiv = minorYDiv = 10;
-        setScale();
-    }
-    /**
-     * Sets the scale for the X and Y axes.
-     */
-    public void setScale() {
-        boolean xRev = false;
         StdDraw.setCanvasSize(canvasWidth, canvasHeight);
-        if (xLower > 0 && xUpper > 0) {                                          // Kink needed.
-            trueXLower = (xLower*canvasWidth - 25*xUpper) / (canvasWidth - 25);
-            trueXUpper = xUpper;
+        StdDraw.setXscale(xLower, (xUpper - xLower)*(200)/(canvasWidth - 200) + xUpper);
+        StdDraw.setYscale(yLower, yUpper);
+        StdDraw.setPenColor(StdDraw.LIGHT_GRAY);
+        
+        for (double x = xLower; x <= xUpper; x += minorXDiv) {
+            StdDraw.line(x, yLower, x, yUpper);
         }
-        else if (xLower == 0 && xUpper > 0) {                                    // No kink needed.
-            trueXLower = (xLower*canvasWidth - 15*xUpper) / (canvasWidth - 15);
-            trueXUpper = xUpper;
+        for (double y = yLower; y <= yUpper; y += minorYDiv) {
+            StdDraw.line(xLower, y, xUpper, y);
         }
-        else if (xLower < 0 && xUpper == 0) {                                    // No kink needed.
-            trueXLower = xLower;
-            //trueXUpper = (xUpper*canvasWidth - 15*xLower) / (canvasWidth - 15);
-            trueXUpper = (xUpper*canvasWidth + 15*xLower) / (canvasWidth - 15);
-            xRev = true;
+        StdDraw.setPenColor(StdDraw.BLACK);
+        for (double x = xLower; x <= xUpper; x += majorXDiv) {
+            StdDraw.line(x, yLower, x, yUpper);
         }
-        else if (xLower < 0 && xUpper < 0) {                                     // Kink needed.
-            trueXLower = xLower;
-            trueXUpper = ((-xUpper*canvasWidth) + 25*xLower) / (canvasWidth - 25);
-            xRev = true;
-        }
-        StdDraw.setXscale(trueXLower, trueXUpper);
-        if (yLower > 0 && yUpper > 0) {
-            trueYLower = (yLower*canvasHeight - 25*yUpper) / (canvasHeight - 25);
-            trueYUpper = yUpper;
-            StdDraw.setYscale(trueYLower, trueYUpper);
-            StdDraw.line(trueXLower, yLower, trueXUpper, yLower);
-        }
-        else if (yLower == 0 && yUpper > 0) {
-            trueYLower = (yLower*canvasHeight - 15*yUpper) / (canvasHeight - 15);
-            trueYUpper = yUpper;   
-            StdDraw.setYscale(trueYLower, trueYUpper);         
-            StdDraw.line(trueXLower, yLower, trueXUpper, yLower);
-        }
-        else if (yLower < 0 && yUpper == 0) {
-            trueYLower = yLower;
-            trueYUpper = (yUpper*canvasHeight + 15*yLower) / (canvasHeight - 15);  
-            StdDraw.setYscale(trueYLower, trueYUpper); 
-            StdDraw.line(trueXLower, yUpper, trueXUpper, yUpper);
-        }
-        else if (yLower < 0 && yUpper < 0) {
-            trueYLower = yLower;
-            trueYUpper = (-yUpper*canvasHeight + 25*yLower) / (canvasHeight - 25);
-            StdDraw.setYscale(trueYLower, trueYUpper);
-            StdDraw.line(trueXLower, yUpper, trueXUpper, yUpper);
-        }
-        if (xRev) 
-            StdDraw.line(xUpper, trueYLower, xUpper, trueYUpper);
-        else
-            StdDraw.line(xLower, trueYLower, xLower, trueYUpper);           
-        //StdDraw.line(trueXLower, yLower, trueXUpper, yLower);
-
+        for (double y = yLower; y <= yUpper; y += majorYDiv) {
+            StdDraw.line(xLower, y, xUpper, y);
+        }        
+        xScale = (xUpper - xLower)/(canvasWidth - 200);
+        yScale = (yUpper - yLower)/(canvasHeight);
+        StdDraw.setFont(new Font("Sans-Serif", Font.PLAIN, 13));
+        StdDraw.setPenRadius(0.002);
+        double textBoxWidth = 50;
+        double textBoxHeight = 13*1.5;
+        StdDraw.rectangle(xUpper + 90*xScale, yUpper - 49*yScale, textBoxWidth/2,13);
+        StdDraw.text(xUpper + 50*xScale, yUpper - 39*yScale, "x: ");
+        StdDraw.text(xUpper + 50*xScale, yUpper - 59*yScale, "y: ");
     }
     /**
      * Set the width represented by 1 major division along the x axis.
@@ -130,7 +96,7 @@ public class StdPlot2D {
      */
     public static void main(String[] args) {
         boolean[] providing = {false, false, true, true, true, true};
-        double[] values = {-200, -10, -400, -20};
+        double[] values = {200, 500, 40, 400};
         StdPlot2D plot = new StdPlot2D(providing, values);
     }
 }
